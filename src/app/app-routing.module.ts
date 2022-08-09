0113e56a873a59092fd724eabf6dc9e5c75f367b
João Pedro Paulino <jppaulino142536@gmail.com>
2022-08-09 08:50:13 -0300

    criando rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', pathMatch: 'full', redirectTo: 'alunos'
+  },
+  { path:'cursos',
+loadChildren: ()=> import('./cursos/cursos.module').then(m => m.CursosModule)
+},
+
+{ path:'alunos',
+loadChildren: ()=> import('./alunos/alunos.module').then(m => m.AlunosModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
